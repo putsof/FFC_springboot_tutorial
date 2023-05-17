82fdc5b3748208184cf269dfc5f3c90c44a0d833
G Ayodele <grace.dele@gmail.com>
2023-05-17 16:32:30 -0400

    first commit

---
+++ amenity-reservation-system/src/main/java/com/amenity_reservation_system/model/AmenityType.java
@@ -0,0 +1,8 @@
+package com.amenity_reservation_system.model;
+
+
+public enum AmenityType {
+
+    AMENITY_TYPE
+
+}
---
+++ amenity-reservation-system/src/main/java/com/amenity_reservation_system/model/ReservationDTO.java
@@ -0,0 +1,34 @@
+package com.amenity_reservation_system.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ReservationDTO {
+
+    private Long id;
+
+    @NotNull
+    private LocalDate reservationDate;
+
+    @NotNull
+    @Schema(type = "string", example = "18:30")
+    private LocalTime startTime;
+
+    @NotNull
+    @Schema(type = "string", example = "18:30")
+    private LocalTime endTime;
+
+    @NotNull
+    private AmenityType amenityType;
+
+    @NotNull
+    private Long user;
+
+}
